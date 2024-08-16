c76102cfd5ec76da9fc0c0eb2737b1667caa6f50
Yannis <13587224594@163.com>
2024-08-16 00:46:28 -0700

    Update WeLift.py

--- WeLift.py
+++ WeLift.py
@@ -111,17 +111,16 @@ def main():
             sns.set_theme(style="whitegrid")
             #cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
             
-            g = sns.relplot(
-                data=data_new,
-                x="EmpID", y="Engagement Score",
-                hue="GenderCode", size="Training Cost",
-                #palette=cmap, 
-                sizes=(10, 200),
+            sns.set_theme(style="whitegrid")
+
+            # Assuming 'EmpID' and 'GenderCode' are columns in your data DataFrame
+            sns.scatterplot(
+                x="EmpID",
+                y="Engagement Score",
+                hue="GenderCode",
+                style="GenderCode",  # Optional: add style for different marker shapes based on GenderCode
+                data=data
             )
-            g.set(xscale="log", yscale="log")
-            g.ax.xaxis.grid(True, "minor", linewidth=.25)
-            g.ax.yaxis.grid(True, "minor", linewidth=.25)
-            g.despine(left=True, bottom=True)
 
             # Highlight new client data point
             if len(new_client) > 0:
